0dea355647dd91826c1442bf7abe018cff07a9e0
arijit-anand <61275226+arijit-anand@users.noreply.github.com>
2022-03-12 21:12:48 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class hello1{
+	
+	public static void main(String[] args)
+	{
+		int i=0;
+		while(i<11)
+		{
+		  System.out.println("The value is here"+i);
+		  i++;
+		}
+	}
